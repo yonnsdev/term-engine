5ad22e574892525fde9938fa67193714c56b9a28
YonnsDev <yonnsdev@gmail.com>
2022-09-12 11:51:56 +0900

    minor changes

--- engine/ecore.c
+++ engine/ecore.c
@@ -49,7 +49,7 @@ void deinitEngine() {
  * @param height  Height of viewport
  * @param fc      Fill character
  */
-void setViewport(int width, int height) {
+void createViewport(int width, int height) {
     CORE.width = width;
     CORE.height = height;
 
